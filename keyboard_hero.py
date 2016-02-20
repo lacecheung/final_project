41a259b0d003e9ef1063640dca7c9ab1ecdbc157
Lace Cheung <lacheung@VISA.COM>
2016-02-20 03:51:29 -0800

    added dynamic blocks 2.20.16

--- keyboard_hero.py
+++ keyboard_hero.py
@@ -8,7 +8,7 @@ pygame.init ()
 
 #game window
 windowwidth = 416
-windowheight = 600
+windowheight = 650
 windowsurface = pygame.display.set_mode((windowwidth, windowheight), 0, 32)
 
 pygame.display.set_caption("Keyboard Hero")
@@ -25,45 +25,41 @@ yellowgray = (150, 150, 80)
 purple = (128, 0, 128)
 
 #move speed (pixels per iteration)
-movespeed = 2
+movespeed = 5
 
 #blocks
-leftblock = {"rect": pygame.Rect(5, 105, 10, 20), "color": green}
-upblock = {"rect": pygame.Rect(107, 207, 10, 20), "color": yellow}
-downblock = {"rect": pygame.Rect(209, 309, 10, 20), "color": blue}
-rightblock = {"rect": pygame.Rect(311, 411, 10, 20), "color": red}
+#NOTE: NEED TO MAKE DIMENSIONS DYNAMICALLY CHANGE DEPENDING ON SIZE OF BOARD
+leftblock = {"rect": pygame.Rect(15, 10, 80, 20), "color": green}
+upblock = {"rect": pygame.Rect(117, 10, 80, 20), "color": yellow}
+downblock = {"rect": pygame.Rect(219, 10, 80, 20), "color": blue}
+rightblock = {"rect": pygame.Rect(321, 10, 80, 20), "color": red}
 
 blocks = [leftblock, upblock, downblock, rightblock]
 
 
-#draw lines
-#guitar "strings"
-vline1 = pygame.draw.line(windowsurface, gray, (4, 5), (4, 595), 2)
-vline2 = pygame.draw.line(windowsurface, gray, (106, 5), (106, 595), 2)
-vline3 = pygame.draw.line(windowsurface, gray, (208, 5), (208, 595), 2)
-vline4 = pygame.draw.line(windowsurface, gray, (310, 5), (310, 595), 2)
-vline5 = pygame.draw.line(windowsurface, gray, (412, 5), (412, 595), 2)
-#event line
-hline1 = pygame.draw.line(windowsurface, yellowgray, (4, 525), (412, 525), 2)
-hline1 = pygame.draw.line(windowsurface, yellowgray, (4, 550), (412, 550), 2)
-#top and bottom border
-hline3 = pygame.draw.line(windowsurface, gray, (4, 5), (412, 5), 2)
-hline4 = pygame.draw.line(windowsurface, gray, (4, 595), (412, 595), 2)
-
-#arrows
-uparrow = pygame.draw.polygon(windowsurface, white, ((45, 560), (45, 567), (70, 567), (70, 580), (45, 580), (45, 587), (35, 573)))
-downarrow = pygame.draw.polygon(windowsurface, white, ((366, 560), (376, 573), (366, 587), (366, 580), (341, 580), (341, 567), (366, 567)))
-
-
-# Rendering surface
-
-
-
+def get_board():
+	#board dynamically changes based on size of board
+	#resets board
+	windowsurface.fill(black)
+	#vertical lines
+	pygame.draw.line(windowsurface, gray, (4, 5), (4, windowheight-5), 2)
+	pygame.draw.line(windowsurface, gray, ((windowwidth-10)/4 + 4, 5), ((windowwidth-10)/4 + 4, windowheight-5), 2)
+	pygame.draw.line(windowsurface, gray, (((windowwidth-10)/4)*2 + 4, 5), (((windowwidth-10)/4)*2 + 4, windowheight-5), 2)
+	pygame.draw.line(windowsurface, gray, (((windowwidth-10)/4)*3 + 4, 5), (((windowwidth-10)/4)*3 + 4, windowheight-5), 2)
+	pygame.draw.line(windowsurface, gray, (windowwidth -5, 5), (windowwidth -5, windowheight-5), 2)
+	#horizontal event lines
+	pygame.draw.line(windowsurface, yellowgray, (4, windowheight-100), (windowwidth-5, windowheight-100), 2)
+	pygame.draw.line(windowsurface, yellowgray, (4, windowheight-50), (windowwidth-5, windowheight-50), 2)
+	#horizontal border
+	pygame.draw.line(windowsurface, gray, (4, 5), (windowwidth-5, 5), 2)
+	pygame.draw.line(windowsurface, gray, (4, windowheight-5), (windowwidth-5, windowheight-5), 2)
+	#arrows
+	pygame.draw.polygon(windowsurface, white, ((45, 610), (45, 617), (70, 617), (70, 630), (45, 630), (45, 637), (35, 623)))
+	pygame.draw.polygon(windowsurface, white, ((366, 560), (376, 573), (366, 587), (366, 580), (341, 580), (341, 567), (366, 567)))
 
 
 
 #execute game
-pygame.display.update()
 
 while True:
 	for event in pygame.event.get():
@@ -71,5 +67,14 @@ while True:
 			pygame.quit()
 			sys.exit()
 
+	get_board()
+
+	for b in blocks:
+		b["rect"].top += movespeed
+
+		pygame.draw.rect(windowsurface, b["color"], b["rect"])
+
+	pygame.display.update()
+	time.sleep(0.02)
 	# resets surface to black 
-	# windowsurface.fill(black)
+	
