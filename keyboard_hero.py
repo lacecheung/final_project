a82987c47283478e7097c90c2f0f292427891c84
Lace Cheung <lacheung@VISA.COM>
2016-02-22 00:52:30 -0800

    working on multiple sets of blocks, and decreasing life points

--- keyboard_hero.py
+++ keyboard_hero.py
@@ -8,10 +8,8 @@ mainclock = pygame.time.Clock()
 
 #iterations per second
 iterationspers = 50
-
-#move speed (pixels per iteration)
-movespeed = 5
-
+#the number of pixels the blocks move per iteration
+movespeed = 4
 
 
 #game window
@@ -33,42 +31,53 @@ yellowgray = (150, 150, 80)
 purple = (128, 0, 128)
 
 
+#key objects:
 
-# minimum iterations (0.02 / iteration) between block sets
-cadence = 200
-
-#blocks
+#colored blocks
 leftblock = {"rect": pygame.Rect(15, 40, 80, 20), "color": green}
 upblock = {"rect": pygame.Rect(117, 40, 80, 20), "color": yellow}
 downblock = {"rect": pygame.Rect(219, 40, 80, 20), "color": blue}
 rightblock = {"rect": pygame.Rect(321, 40, 80, 20), "color": red}
 
+#area where the player must take an action to gain points
+lefteventbox = {"rect": pygame.Rect(5, windowheight-99, (windowwidth-10)/4 - 1, 50), "color": black}
+upeventbox = {"rect": pygame.Rect((windowwidth-10)/4 + 5, windowheight-99, (windowwidth-10)/4 - 1, 50), "color": black}
+downeventbox = {"rect": pygame.Rect(((windowwidth-10)/4)*2 + 5, windowheight-99, (windowwidth-10)/4 - 1, 50), "color": black}
+righteventbox = {"rect": pygame.Rect(((windowwidth-10)/4)*3 + 5, windowheight-99, (windowwidth-10)/4 + 1, 50), "color": black}
+
+#show how much life the player has
 lifepoints = pygame.Rect(windowwidth - 203, 9, 197, 17)
 
-#eventline
-eventline = windowheight - 45
+#end of the event area
+bottomeventline = windowheight - 47
+
+
 
 #redraw board each time so block trail does not show
 def get_board():
-	#board dynamically changes based on size of board
 	#resets board
 	windowsurface.fill(black)
-	#vertical lines
+
+	#draw vertical lines
 	pygame.draw.line(windowsurface, gray, (4, 35), (4, windowheight-5), 2)
 	pygame.draw.line(windowsurface, gray, ((windowwidth-10)/4 + 4, 35), ((windowwidth-10)/4 + 4, windowheight-5), 2)
 	pygame.draw.line(windowsurface, gray, (((windowwidth-10)/4)*2 + 4, 35), (((windowwidth-10)/4)*2 + 4, windowheight-5), 2)
 	pygame.draw.line(windowsurface, gray, (((windowwidth-10)/4)*3 + 4, 35), (((windowwidth-10)/4)*3 + 4, windowheight-5), 2)
 	pygame.draw.line(windowsurface, gray, (windowwidth -5, 35), (windowwidth -5, windowheight-5), 2)
-	#horizontal event lines
+
+	#draw horizontal event lines
 	pygame.draw.line(windowsurface, yellowgray, (4, windowheight-100), (windowwidth-5, windowheight-100), 2)
 	pygame.draw.line(windowsurface, yellowgray, (4, windowheight-50), (windowwidth-5, windowheight-50), 2)
-	#horizontal border
+
+	#draw horizontal border
 	pygame.draw.line(windowsurface, gray, (4, 35), (windowwidth-5, 35), 2)
 	pygame.draw.line(windowsurface, gray, (4, windowheight-5), (windowwidth-5, windowheight-5), 2)
-	#arrows
+
+	#draw arrows
 	pygame.draw.polygon(windowsurface, white, ((45, 610), (45, 617), (70, 617), (70, 630), (45, 630), (45, 637), (35, 623)))
 	pygame.draw.polygon(windowsurface, white, ((366, 610), (376, 623), (366, 637), (366, 630), (341, 630), (341, 617), (366, 617)))
-	#Life bar
+
+	#draw life bar
 	pygame.draw.rect(windowsurface, gray, (windowwidth - 205, 7, 200, 20), 2)
 
 
@@ -78,7 +87,6 @@ def blocksperline():
 
 
 #generate which blocks to generate per line
-
 def get_blocks():
 	listblocks = []
 	blocks = [leftblock, upblock, downblock, rightblock]
@@ -91,17 +99,17 @@ def get_blocks():
 
 	return listblocks
 
-# How often to generate new blocks
 
+# How often to generate new blocks
 def show_blocks():
 	return random.choice([True, False, False, False])
 
 
 #execute game
 
-
-combo = {}
-seconds = 0
+blockcombo = {}
+iteration = 0
+currentblocks = []
 
 
 while True:
@@ -110,44 +118,42 @@ while True:
 			pygame.quit()
 			sys.exit()
 
+	# resets surface to black 
 	get_board()
 
-	combo[seconds] = get_blocks()
+	if iteration%50 == 0:
+		currentblocks.append(iteration)
+		blockcombo[iteration] = get_blocks()
 
-	#new part
+	# for every set of blocks currently showing
+	for iteration in currentblocks:
 
-	# lineswithblocks = []
+		# for each block in that set of blocks, move the block downwards at movespeed
+		for b in blockcombo[iteration]:
+			b["rect"].top += movespeed
+		
+			# If blocks cross the bottom event line, shrink height of block
+			if b["rect"].bottom >= bottomeventline:
+				b["rect"].height -= movespeed
 
-	# if seconds%100 == 0 and show_blocks() == True:
-	# 	lineswithblocks.append(seconds)
+			# blocks disappear when height is zero, and remove it from currentblocks list
+			if b["rect"].height <= 0: 
+				currentblocks.remove(iteration)
 
-	# 	for line in lineswithblocks:
+				if lifepoints.width >= 0:
+					lifepoints.width -= 5
 
+				break
 
-	# render blocks moving down at variable movespeed
-	#NOTE: HOW TO DUPLICATE BELOW CODE FOR EVERY NEW SET OF BLOCKS?
-
-	for b in combo[0]:
-		b["rect"].top += movespeed
-	
-
-		# If blocks cross the bottom event line, shrink height of block
-		if b["rect"].bottom >= eventline:
-			b["rect"].height -= movespeed
-
-			# blocks disappear when height is zero
-		if b["rect"].height <= 0: 
-			break
-
-		pygame.draw.rect(windowsurface, b["color"], b["rect"])
+			pygame.draw.rect(windowsurface, b["color"], b["rect"])
 
 	pygame.draw.rect(windowsurface, green, lifepoints)
 
-	seconds += 1
+	iteration += 1
 
 	pygame.display.update()
-	
+
 	mainclock.tick(iterationspers)
 	
-	# resets surface to black 
+
 	
